bf71649a39cb1289ad54250934ed4adf23cc60cb
thatwh0came <133685029+thatwh0came@users.noreply.github.com>
2023-05-16 18:13:28 +0300

    add code

---
+++ botixa.py
@@ -0,0 +1,77 @@
+import telebot
+from telebot import types
+import sqlite3
+import datetime
+
+
+months = {
+    1: 'января',
+    2: 'февраля',
+    3: 'марта',
+    4: 'апреля',
+    5: 'мая',
+    6: 'июня',
+    7: 'июля',
+    8: 'августа',
+    9: 'сентября',
+    10: 'октября',
+    11: 'ноября',
+    12: 'декабря'
+}
+
+conn = sqlite3.connect('Stat_data_base.db')
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM stats')
+rows = cursor.fetchall()
+
+bot = telebot.TeleBot("5556001810:AAHC5EOqRnorGiuYHJyY4rL_ntgMr131Jbk")
+bot.set_webhook()
+
+menu_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+itembtn1 = types.KeyboardButton('Присылать результаты')
+menu_keyboard.add(itembtn1)
+
+@bot.message_handler(commands=['start'])
+def welcome_message(message):
+    username = message.from_user.username
+    if username:
+        bot.send_message(chat_id=message.chat.id, text = f"Здаров заебал, {username}\nЯ буду присылать тебе результаты матчей Национальной Баскетбольной Ассоциации")
+    else:
+        bot.send_message(chat_id=message.chat.id, text = f"Здаров, лысый\nЯ буду присылать тебе результаты твоих любимых команд Национальной Баскетбольной Ассоциации")
+    bot.send_message(message.chat.id, "Нажми на кнопку, чтобы начать получать рассылку результатов", reply_markup=menu_keyboard)
+
+
+@bot.message_handler(commands=['data'])
+def send_data(message):
+    now = datetime.datetime.now()
+    day = now.day
+    month = now.month
+    month_name = months[month]
+    date_message = f"{day} {month_name}"
+    bot.send_message(message.chat.id, f"⬇️Результаты матчей за {date_message}⬇️")
+    for row in rows:
+        t1 = row[0]
+        t2 = row[3]
+        r1 = row[1]
+        r2 = row[2]
+        tp1 = row[4]
+        tp2 = row[5]
+        tps1 = row[6]
+        tps2 = row[7]
+        rec1 = row[8]
+        rec2 = row[9]
+        # Объединение значений в одну строку
+        combined_message = f"""{t1} ({rec1}): {r1}
+{t2} ({rec2}): {r2}
+⭐️{tp1}: {tps1}
+⭐️{tp2}: {tps2}"""
+        bot.send_message(message.chat.id, combined_message)
+
+
+@bot.message_handler(func=lambda message: True)
+def handle_callback(message):
+    if message.text == 'Присылать результаты':
+        bot.send_message(message.chat.id, "Рассылка запущена", reply_markup=types.ReplyKeyboardRemove())
+
+
+bot.polling()
---
+++ pd_to_db.py
@@ -0,0 +1,124 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import sqlite3 as sq
+import schedule 
+import time
+
+
+
+def parc():
+	all_teams = []
+	team1 = []
+	team2 = []
+	all_res = []
+	res_team1 = []
+	res_team2 = []
+	all_beasts = []
+	beast_team1 = []
+	beast_team2 = []
+	stat_all = []
+	stat1 = []
+	stat11 = []
+	stat2 = []
+	stat22 = []
+	all_rec = []
+	rec_n = []
+	rec1 = []
+	rec2 = []
+
+
+	url = "https://www.espn.com/nba/scoreboard/_/date/20230215"
+
+
+	r = requests.get(url).text
+
+	soup = BeautifulSoup(r, 'html5lib')
+
+
+
+	teams = soup.find_all('div', class_ = 'ScoreCell__Truncate clr-gray-01 ScoreCell__Truncate--scoreboard flex items-center h7 h5')
+	for tms in teams:
+		all_teams += [tms.text]
+		#print(all_teams)
+	for i in range(0, len(all_teams), 2):
+		team1 += [all_teams[i]]
+		#print(team1)
+	for i in range(1, len(all_teams), 2):
+		team2 += [all_teams[i]]
+		#print(team2)
+
+
+	rs = soup.find_all('div', class_ = 'ScoreCell__Score h4 clr-gray-01 fw-heavy tar ScoreCell_Score--scoreboard pl2')
+	for R in rs:
+		all_res += [R.text]
+	for i in range(0, len(all_res), 2):
+		res_team1 += [all_res[i]]
+	for i in range(1, len(all_res), 2):
+		res_team2 += [all_res[i]]
+
+
+
+
+	tp = soup.find_all('span', class_ = 'Athlete__PlayerName')
+	for t in tp:
+		all_beasts += [t.text]
+	#print(all_beasts)
+	for i in range(0, len(all_beasts), 2):
+		beast_team1 += [all_beasts[i]]
+	for i in range(1, len(all_beasts), 2):
+		beast_team2 += [all_beasts[i]]
+
+	#print(list_beasts)
+
+
+	tp_s = soup.find_all('div', class_ = 'Athlete__Stats mt2 clr-gray-04 ns9')
+	for tp in tp_s:
+		stat_all += [tp.text]
+	for i in range(0, len(stat_all), 2):
+		stat1 += [stat_all[i]] 
+	for i in range(1, len(stat_all), 2):
+		stat2 += [stat_all[i]] 
+	for i in range(len(stat1)):
+		el = str(stat1[i])
+		if 'PTS' in el or 'REB' in el or 'AST' in el or 'STL' in el or 'BLK' in el:
+			el = el.replace('PTS', ' PTS ').replace('REB', ' REB ').replace('STL', ' STL ').replace('BLK',' BLK ').replace('AST', ' AST ')
+		stat11.append(el)
+	for i in range(len(stat2)):
+		el = str(stat2[i])
+		if 'PTS' in el or 'REB' in el or 'AST' in el or 'STL' in el or 'BLK' in el:
+			el = el.replace('PTS', ' PTS ').replace('REB', ' REB ').replace('STL', ' STL ').replace('BLK',' BLK ').replace('AST', ' AST ')
+		stat22.append(el)
+
+	
+
+	rec = soup.find_all('span', class_ = 'ScoreboardScoreCell__Record')
+	for r in rec:
+		all_rec += [r.text]
+	for i in range(0, len(all_rec), 2):
+		rec_n += [all_rec[i]]
+	for i in range(0, len(rec_n), 2):
+		rec1 += [rec_n[i]]
+	for i in range(1, len(rec_n), 2):
+		rec2 += [rec_n[i]]
+
+	list_all = list(zip(team1,res_team1, res_team2, team2, beast_team1, beast_team2, stat11, stat22, rec1, rec2))
+	df = pd.DataFrame(list_all, columns = ['Team1','Score1','Score2', 'Team2', 'Top_Performance1','Top_Performance2', 'Top_Performance_Score1', 'Top_Performance_Score2', 'Record1', 'Record2'])
+	#print(df)
+
+
+
+	conn = sq.connect('Stat_data_base.db')
+	df.to_sql("stats",conn, if_exists='replace', index=False)
+	conn.close()
+
+parc()
+
+
+
+
+'''
+schedule.every().day.at("19:40").do(parc)
+while True:
+   schedule.run_pending()
+   time.sleep(1)'''
